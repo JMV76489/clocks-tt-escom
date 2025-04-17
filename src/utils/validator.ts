3468156211eaa5a7242821cda750a24821269e2a
JMV76489 <josuemiguel450@gmail.com>
2025-04-16 21:19:03 -0600

    Añadir validación para identificadores validos.

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -1,16 +1,38 @@
 /* ------------------ Archivo de definicion de validadores ------------------ */
 
-import { C_PRIMITIVE_DATATYPES, C_RESERVED_KEYWORDS } from "./constants";
+import { C_PRIMITIVE_DATATYPES, C_RESERVED_IDENTIFIERS, C_RESERVED_KEYWORDS, MATH_CONSTANTS_CODE_DICT, MATH_H_FUNCTIONS_NAME_CODE_DICT, OPERATION_BINARY_BASIC_NAME_CODE_DICT } from "./constants";
 
 //Expresión regular para validar campo de indetificador
 const regex_variable_identifier = new RegExp("^[_a-zA-Z][_a-zA-Z0-9]{0,30}$") 
 
+
+//Función para validar si el identificador es utilizable
+function isIdentifierUsed(fieldValue: string){
+    for(const key in C_RESERVED_IDENTIFIERS){
+        const curIdentifier = C_RESERVED_IDENTIFIERS[key];
+        if (fieldValue == curIdentifier) 
+            return true;
+    }
+    for(const key in MATH_CONSTANTS_CODE_DICT){
+        const curConstant = MATH_CONSTANTS_CODE_DICT[key];
+        if (fieldValue == curConstant) 
+            return true;
+    }
+    for(const key in MATH_H_FUNCTIONS_NAME_CODE_DICT){
+        const curConstant = MATH_H_FUNCTIONS_NAME_CODE_DICT[key];
+        if (fieldValue == curConstant) 
+            return true;
+    }
+    return false;
+}
+
 //Validador de campo de de identificador
 export const identifierValidator = function(fieldValue: string){
+    fieldValue = fieldValue.trim(); //Recortar espacios al inicio y al final de la cadena
     //Validar si la sintaxis es correcta
     const isSyntaxCorrect = regex_variable_identifier.test(fieldValue) 
     //Verificar que el tenga sintaxis correcta y no use ninguna palabra clave
-    if (!isSyntaxCorrect || C_RESERVED_KEYWORDS.indexOf(fieldValue) > -1) {
+    if (!isSyntaxCorrect || C_RESERVED_KEYWORDS.indexOf(fieldValue) > -1 || isIdentifierUsed(fieldValue)) {
         return null;
     }
     return fieldValue
