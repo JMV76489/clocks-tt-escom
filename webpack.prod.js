4649fecfdeddfa899b8cef3514273d91b07008ef
JMV76489 <josuemiguel450@gmail.com>
2025-04-27 19:46:04 -0600

    Adición de mejor configuración de chunks.

--- webpack.prod.js
+++ webpack.prod.js
@@ -11,10 +11,29 @@ module.exports = merge(common, {
     mode: 'production', //Modo de producción
     optimization: {
         usedExports: true, //Eliminar el código muerto que no se utiliza en la aplicación
+        //Dividir el código en diferentes archivos para optimizar la carga
         splitChunks: {
-            chunks: 'all', //Dividir el código en diferentes archivos para optimizar la carga
+            chunks: 'async',
+            minSize: 20000,
+            minRemainingSize: 0,
+            minChunks: 1,
+            maxAsyncRequests: 30,
+            maxInitialRequests: 30,
+            enforceSizeThreshold: 50000,
+            cacheGroups: {
+                defaultVendors: {
+                  test: /[\\/]node_modules[\\/]/,
+                  priority: -10,
+                  reuseExistingChunk: true,
+                },
+                default: {
+                  minChunks: 2,
+                  priority: -20,
+                  reuseExistingChunk: true,
+                },
+            },
         },
-        runtimeChunk: 'single', //Crear un archivo de código de ejecución único para optimizar la carg
+        runtimeChunk: 'single', //Crear un archivo de código de ejecución único para optimizar la carga
         minimize: true, //Minificar el código para reducir el tamaño del bundle
         //Utilizar el plugin TerserPlugin para minificar el código
         minimizer: [
@@ -29,8 +48,8 @@ module.exports = merge(common, {
     },
     performance: {
         hints: false, //Desactivar las advertencias de tamaño de los archivos generados
-        maxEntrypointSize: 512000, //Establecer un tamaño máximo de 512KB para los archivos generados
-        maxAssetSize: 512000, //Establecer un tamaño máximo de 512KB para los archivos generados
+        maxEntrypointSize: 512000, //Establecer un tamaño máximo de 512KB para los archivos de entrada
+        maxAssetSize: 512000, //Establecer un tamaño máximo de 512KB para los archivos emitidos por webpack
     },
     output: {
         //Nombre del archivo de salida
